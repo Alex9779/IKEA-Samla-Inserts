6649fc8b3fa4dab275b98fcd58a9662d577fe885
Alexander Leisentritt <alexander.leisentritt@solidline.de>
2020-03-26 21:01:35 +0100

    add wall and bottom width to output files name

--- make_files_11l.cmd
+++ make_files_11l.cmd
@@ -12,8 +12,8 @@ SET layers=3
 SET wall=0.84
 SET bott=0.57
 FOR /L %%L IN (1,1,%layers%) DO (
-ECHO Generating 'IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%.stl'
-"C:\Program Files\OpenSCAD\openscad.com" -o IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%.stl -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;" IKEA_Samla_Inserts.scad
+ECHO Generating 'IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl'
+"C:\Program Files\OpenSCAD\openscad.com" -o IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;" IKEA_Samla_Inserts.scad
 )
 
 REM REM 3x3 4 layers
@@ -24,6 +24,6 @@ SET layers=4
 SET wall=0.84
 SET bott=0.57
 FOR /L %%L IN (1,1,%layers%) DO (
-ECHO Generating 'IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%.stl'
-"C:\Program Files\OpenSCAD\openscad.com" -o IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%.stl -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;" IKEA_Samla_Inserts.scad
+ECHO Generating 'IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl'
+"C:\Program Files\OpenSCAD\openscad.com" -o IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;" IKEA_Samla_Inserts.scad
 )
--- make_files_22l.cmd
+++ make_files_22l.cmd
@@ -12,6 +12,6 @@ SET layers=4
 SET wall=0.84
 SET bott=0.57
 FOR /L %%L IN (1,1,%layers%) DO (
-ECHO Generating 'IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%.stl'
-"C:\Program Files\OpenSCAD\openscad.com" -o IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%.stl -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;" IKEA_Samla_Inserts.scad
+ECHO Generating 'IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl'
+"C:\Program Files\OpenSCAD\openscad.com" -o IKEA_Samla_Inserts_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;" IKEA_Samla_Inserts.scad
 )
