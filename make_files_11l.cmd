81782c12399dc1ae8de047c010eab159d337d3d0
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2020-03-30 13:38:05 +0200

    changed filenames for created files to get better ordering

--- make_files_11l.cmd
+++ make_files_11l.cmd
@@ -14,7 +14,7 @@ SET wall=0.88
 SET bott=0.87
 SET halve=column
 FOR /L %%L IN (1,1,%layers%) DO (
-SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl
+SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%wall%x%bott%_%%L-%layers%.stl
 ECHO Generating '!filename!'
 "C:\Program Files\OpenSCAD\openscad.com" -o !filename! -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;Halve=\"%halve%\";Test=\"false\"" IKEA_Samla_Inserts.scad
 )
@@ -28,7 +28,7 @@ SET wall=0.88
 SET bott=0.87
 SET halve=column
 FOR /L %%L IN (1,1,%layers%) DO (
-SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl
+SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%wall%x%bott%_%%L-%layers%.stl
 ECHO Generating '!filename!'
 "C:\Program Files\OpenSCAD\openscad.com" -o !filename! -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;Halve=\"%halve%\";Test=\"false\"" IKEA_Samla_Inserts.scad
 )
--- make_files_5l.cmd
+++ make_files_5l.cmd
@@ -13,7 +13,7 @@ SET layers=2
 SET wall=0.88
 SET bott=0.87
 FOR /L %%L IN (1,1,%layers%) DO (
-SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl
+SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%wall%x%bott%_%%L-%layers%.stl
 ECHO Generating '!filename!'
 "C:\Program Files\OpenSCAD\openscad.com" -o !filename! -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;Halve=\"false\";Test=\"false\"" IKEA_Samla_Inserts.scad
 )
@@ -26,7 +26,7 @@ SET layers=3
 SET wall=0.88
 SET bott=0.87
 FOR /L %%L IN (1,1,%layers%) DO (
-SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl
+SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%wall%x%bott%_%%L-%layers%.stl
 ECHO Generating '!filename!'
 "C:\Program Files\OpenSCAD\openscad.com" -o !filename! -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;Halve=\"false\";Test=\"false\"" IKEA_Samla_Inserts.scad
 )
@@ -39,7 +39,7 @@ SET layers=4
 SET wall=0.88
 SET bott=0.87
 FOR /L %%L IN (1,1,%layers%) DO (
-SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl
+SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%wall%x%bott%_%%L-%layers%.stl
 ECHO Generating '!filename!'
 "C:\Program Files\OpenSCAD\openscad.com" -o !filename! -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;Halve=\"false\";Test=\"false\"" IKEA_Samla_Inserts.scad
 )
@@ -52,7 +52,7 @@ SET layers=10
 SET wall=0.88
 SET bott=0.87
 FOR /L %%L IN (1,1,%layers%) DO (
-SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%%L-%layers%_%wall%x%bott%.stl
+SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%wall%x%bott%_%%L-%layers%.stl
 ECHO Generating '!filename!'
 "C:\Program Files\OpenSCAD\openscad.com" -o !filename! -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;Halve=\"false\";Test=\"false\"" IKEA_Samla_Inserts.scad
 )
