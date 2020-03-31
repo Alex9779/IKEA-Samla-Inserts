c4c962d1f9b109b5dbf0e07f21d9bf782b073e22
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2020-03-31 11:24:31 +0200

    added selection of "halve" to the script

--- make_files.cmd
+++ make_files.cmd
@@ -9,10 +9,11 @@ SET /p boxsize="Enter box size (5, 11, 22): "
 SET /p cols="Columns: "
 SET /p rows="Rows: "
 SET /p layers="Layers: "
+SET /p halve="Halve (false, column, row): "
 SET /p wall="Wall thickness: "
 SET /p bott="Bottom thickness: "
 FOR /L %%L IN (1,1,%layers%) DO (
 SET filename=IKEA_Samla_Inserts_%boxsize%l_%cols%x%rows%_%wall%x%bott%_%%L-%layers%.stl
 ECHO Generating '!filename!'
-"C:\Program Files\OpenSCAD\openscad.com" -o !filename! -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;Halve=\"false\";Test=\"false\"" IKEA_Samla_Inserts.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o !filename! -D "Box_Size=\"%boxsize%\";Active_Layer=%%L;Layers=%layers%;Cell_Columns=%cols%;Cell_Rows=%rows%;Resolution=%res%;Wall_Thickness=%wall%;Bottom_Thickness=%bott%;Halve=\"%halve%\";Test=\"false\"" IKEA_Samla_Inserts.scad
 )
